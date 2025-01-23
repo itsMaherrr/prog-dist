067a90aaa8df1dce5983a86ec652f5fa61fc8dbe
itsMaherrr <mahermebirouk@gmail.com>
2025-01-23 11:56:00 +0100

    Initial commit: Spring Boot RESTful App

---
+++ src/main/java/com/example/restcicd/data/Dates.java
@@ -0,0 +1,30 @@
+package com.example.restcicd.data;
+
+public class Dates {
+    String begin;
+    String end;
+
+    public String getBegin() {
+        return begin;
+    }
+
+    public void setBegin(String begin) {
+        this.begin = begin;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "Dates{" +
+                "begin='" + begin + '\'' +
+                ", end='" + end + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/restcicd/web/RentalWebService.java
@@ -0,0 +1,54 @@
+package com.example.restcicd.web;
+
+import com.example.rentalService.data.Car;
+import com.example.rentalService.data.Dates;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class RentalWebService {
+
+    List<Car> cars = new ArrayList<Car>();
+
+    Logger logger = LoggerFactory.getLogger(RentalWebService.class);
+
+    public RentalWebService() {
+        Car car = new Car("11AA22", 2000);
+        cars.add(car);
+    }
+
+    @GetMapping("/cars")
+    public List<Car> getCars(){
+        return cars;
+    }
+
+    @PutMapping(value = "/cars/{plaque}")
+    public void rent(
+            @PathVariable("plaque") String plateNumber,
+            @RequestParam(value="rent", required = true)boolean rent,
+            @RequestBody Dates dates) throws CarNotFoundException {
+
+        logger.info("Plate number: " + plateNumber);
+        logger.info("Rent: " + rent);
+        logger.info("Dates: " + dates);
+
+        Car car = cars.stream().filter(aCar -> aCar.getPlateNumber().equals(plateNumber)).findFirst().orElse(null);
+        if(car != null){
+            if(rent == true){
+                car.setRented(true);
+                car.getDates().add(dates);
+            } else {
+                car.setRented(false);
+            }
+        } else {
+            logger.error("Car not found: " + plateNumber);
+            throw new CarNotFoundException(plateNumber);
+        }
+
+    }
+
+}
