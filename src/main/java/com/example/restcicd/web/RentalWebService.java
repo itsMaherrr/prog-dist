31c18cec6437a3b2324f3640e2a170c0b0b27429
itsMaherrr <mahermebirouk@gmail.com>
2025-01-23 12:21:09 +0100

    car deleted

--- src/main/java/com/example/restcicd/web/RentalWebService.java
+++ src/main/java/com/example/restcicd/web/RentalWebService.java
@@ -19,8 +19,6 @@ public class RentalWebService {
     public RentalWebService() {
         Car car = new Car("11AA22", 2000);
         cars.add(car);
-        car = new Car("Mercedes-AMG", 15000);
-        cars.add(car);
     }
 
     @GetMapping("/cars")
